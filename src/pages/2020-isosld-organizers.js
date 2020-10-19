331e5da34a9d16f37fa2fe76115396399c48e223
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2020-10-19 18:29:57 -0400

    added 2020 isosld organizers

---
+++ src/pages/2020-isosld-organizers.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Script from "react-load-script"
+
+export default function Home() {
+  return (
+    <>
+      <div className="klaviyo-form-RqVk9M"></div>
+      <Script url="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=JXzNvL" />
+    </>
+  )
+}
